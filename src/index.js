cb036001fdd4c0126e77c9eecf7d40e0fee5ae2b
CodeEMP <cpgameface@gmail.com>
2018-06-16 15:27:13 -0500

    serviceworker added

--- src/index.js
+++ src/index.js
@@ -3,8 +3,25 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import 'bulma/css/bulma.css'
+import {getContacts, setContacts} from './Contacts.js'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+function fetchFailed () {
+  console.error('Uh-oh - fetching initial inventory.json failed!')
+}
+
+if (!getContacts()) {
+  window.fetch('data/contacts.json')
+    .then(function (response) { return response.json() })
+    .then(setContacts)
+    .then(startRendering)
+    .catch(fetchFailed)
+} else {
+  startRendering()
+}
+
+function startRendering () {
+  ReactDOM.render(<App />, document.getElementById('root'))
+}
+
+registerServiceWorker()
